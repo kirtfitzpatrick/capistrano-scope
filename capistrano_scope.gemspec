870b8ad77f3c7f8ccd80e06006181a239a177624
Kirt Fitzpatrick <kirt.fitzpatrick@SFM523J20GW.local>
2010-09-17 13:40:23 -0700

    initial commit.

---
+++ capistrano_scope.gemspec
@@ -0,0 +1,22 @@
+
+Gem::Specification.new do |s|
+  s.name                  = 'capistrano-scope'
+  s.rubyforge_project     = 'capistrano-scope'
+  s.homepage              = 'https://rubygems.org/gems/capistrano-scope'
+  s.version               = '0.0.6'
+  s.summary               = 'Lightweight command line server selection for capistrano.'
+  s.description           = 'If puppet were a Cadillac, capistrano-scope would be a bicycle.'
+
+  s.required_ruby_version = '>= 1.8.6'
+
+  s.author                = 'Kirt Fitzpatrick'
+  s.email                 = 'kirt.fitzpatrick@akqa.com'
+
+  s.files                 = Dir['README', 'lib/capistrano/scope.rb']
+  s.rdoc_options << '--title' << 
+      'Capistrano Scope - Lightweight command line server selection.' <<
+      '--main' << 'README' << '--line-numbers'
+  s.extra_rdoc_files      = Dir['README']
+
+  s.add_dependency('capistrano', '2.5.18')
+end
---
+++ lib/capistrano/scope.rb
@@ -0,0 +1,112 @@
+# require 'capistrano'
+# unless Capistrano::Configuration.respond_to?(:instance)
+#   abort "scope requires requires Capistrano 2"
+# end
+
+
+module Scope
+  @@servers = Hash.new {|h,k| h[k] = {:ip_address => nil, :roles => [] } }
+
+  def role(role, server_name, options={})
+    @@servers[server_name][:roles] << {:name => role, :options => options}
+  end
+
+  def server(server_name, ip_address)
+    @@servers[server_name][:ip_address] = ip_address
+  end
+
+  def create_task(name, pattern, ns)
+    ns.task(name) do
+      keys = @@servers.keys.grep(/^#{pattern}/)
+      
+      keys.each do |key|
+        ip_address = @@servers[key][:ip_address]
+        @@servers[key][:roles].each do |role|
+          top.role(role[:name], ip_address, role[:options])
+        end
+      end
+    end
+  end
+  
+  def namespace_task(namespace_list, ns)
+    name    = namespace_list.pop
+    pattern = "#{ns.fully_qualified_name}:#{name}".sub('@:','')
+
+    if namespace_list.empty?
+      create_task name, pattern, ns
+    else
+      ns.namespace name.to_sym do
+      end
+      create_task 'all', pattern, ns.namespaces[name.to_sym]
+      namespace_task namespace_list, ns.namespaces[name.to_sym]
+    end 
+  end
+
+  # Execution starts here.
+  def create_role_tasks(ns)
+    @@servers.keys.each do |name| 
+      namespace_task name.split(':').reverse, ns
+    end
+  end
+  
+  def all(ns)
+    @@servers.each_value do |scope_server|
+      scope_server[:roles].each do |role|
+        ns.top.role(role[:name], scope_server[:ip_address], role[:options])
+      end
+    end
+  end
+  
+  def defined_roles(ns)
+    ns.top.roles.keys.collect {|r| r.to_s }.sort {|a,b| a <=> b }
+  end
+
+  def defined_servers(ns)
+    ip_to_name = @@servers.keys.inject({}) do |h,key| 
+      h[@@servers[key][:ip_address]] = key
+      h
+    end
+    defined_servers = []
+    ns.top.roles.each_value do |defined_role|
+      defined_role.servers.each do |server|
+        unless defined_servers.include? ip_to_name[server.host]
+          defined_servers << ip_to_name[server.host]
+        end
+      end
+    end
+
+    return defined_servers.sort
+  end
+end
+Capistrano.plugin :scope, Scope
+
+
+Capistrano::Configuration.instance.load do
+  namespace '@'.to_sym do
+
+    task :create_role_tasks do
+      scope.create_role_tasks self
+    end
+
+    desc "Scope all defined servers"
+    task :all do
+      scope.all self
+    end
+  
+    desc "Show the roles and servers defined by this scope."
+    task :show do
+      puts "Roles:"
+      scope.defined_roles(self).each do |defined_role|
+        puts "         #{defined_role}"
+      end
+      puts "Servers:"
+      scope.defined_servers(self).each do |defined_server|
+        puts "         #{defined_server}"
+      end
+    end
+
+  end
+
+  on :load, '@:create_role_tasks'
+end
+
